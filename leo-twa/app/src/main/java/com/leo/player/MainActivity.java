db2e1d4a099fa4f51ce714304802ac4d9c2ffbe9
COOLEO1 <Leoc39063@gmail.com>
2025-12-15 14:01:58 +0200

    Flatten submodule and include gradlew directly

---
+++ leo-twa/app/src/main/java/com/leo/player/MainActivity.java
@@ -0,0 +1,9 @@
+package com.leo.player;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.androidbrowserhelper.trusted.LauncherActivity;
+
+public class MainActivity extends LauncherActivity {
+    // No need to override anything — LauncherActivity handles everything
+}
